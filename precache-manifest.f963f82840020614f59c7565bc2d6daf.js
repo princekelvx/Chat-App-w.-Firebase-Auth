de89dd7506ad9e17a14ab5ab532abfab1f595327
princekelvx@gmail.com <[princekelvx@gmail.com]>
2022-03-21 05:30:46 -0700

    Initial Commit

---
+++ precache-manifest.f963f82840020614f59c7565bc2d6daf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8642e5cc4c8db3af998ea79730b147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9116838cd74f35f6c8ad",
+    "url": "/static/css/main.56ba4584.chunk.css"
+  },
+  {
+    "revision": "0b571e6a7e4bb9c29d89",
+    "url": "/static/js/2.7871f936.chunk.js"
+  },
+  {
+    "revision": "ce6f5f56f01747a0592e5a892783e333",
+    "url": "/static/js/2.7871f936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9116838cd74f35f6c8ad",
+    "url": "/static/js/main.c4a5fb57.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
